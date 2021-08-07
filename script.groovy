7143d62995f3869bf2a59e887344fa49a003aa77
SAIDA D <softpro.saida@gmail.com>
2021-08-07 15:20:16 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,20 @@
+// block could be your git repo
 def stageCheckout(){
   echo "Repository checkout stage - Completed"
 }
 
+// block for build stage 
 def stageBuild(){
   echo "Build stage - Completed"
 }
 
+// block for testing stage
 def stageTest(){
   echo "Test stage - Completed"
 
 }
+
+// block for production deploy /k8s/docker swarm deploy
 def stageProdDeploy(){
   echo "Production deploy - Completed"
 
