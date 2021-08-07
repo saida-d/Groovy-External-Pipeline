c375096423c50f4f2d40ec969e8bd5d0eaa9ea85
SAIDA D <softpro.saida@gmail.com>
2021-08-07 15:18:38 +0800

    Update script.groovy
    Updated sample functions

--- script.groovy
+++ script.groovy
@@ -1,3 +1,18 @@
-def stageBuild(){
-  echo "Build stage done"
+def stageCheckout(){
+  echo "Repository checkout stage - Completed"
 }
+
+def stageBuild(){
+  echo "Build stage - Completed"
+}
+
+def stageTest(){
+  echo "Test stage - Completed"
+
+}
+def stageProdDeploy(){
+  echo "Production deploy - Completed"
+
+}
+
+
